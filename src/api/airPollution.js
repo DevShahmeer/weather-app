cc48559d99d6f4458627a9f1ea5f7a7ca1050a6c
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-03 23:50:07 +0500

    Add AirPollution API functions

---
+++ src/api/airPollution.js
@@ -0,0 +1,13 @@
+import { fetchData } from "./common";
+import { ERROR_INVALID_LAT_LON, BASE_URL_WEATHER } from "../utils/constants";
+
+export const getAirPollutionByLatLon = async (lat_, lon_) => {
+    if (!lat_ || !lon_) {
+        const error = { message: ERROR_INVALID_LAT_LON };
+        throw error;
+    }
+
+    const baseUrl = BASE_URL_WEATHER + "/air_pollution";
+    const params = `lat=${lat_}&lon=${lon_}`;
+    return await fetchData(baseUrl, params);
+    };
