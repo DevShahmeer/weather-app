1c93af8a0bc9a2958e6570efc58dfb9219c07087
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-03 23:47:28 +0500

    Add forecast API functions

---
+++ src/api/forecast.js
@@ -0,0 +1,24 @@
+import { fetchData } from "./common";
+import { ERROR_INVALID_CITY, ERROR_INVALID_LAT_LON, BASE_URL_WEATHER } from "../utils/constants";
+
+export const getForecastByLatLon = async (lat_, lon_) => {
+    if(!lat_ || !lon_){
+        const error = { message: ERROR_INVALID_LAT_LON};
+        throw(error);
+    }
+
+    const baseUrl = BASE_URL_WEATHER +"/forecast";
+    const params = `lat=${lat_}&lon=${lon_}`;
+    return await fetchData(baseUrl, params);
+}
+
+export const getForecastByCity = async (city_) => {
+    if(!city_ || city_ === "" || city_ === " "){
+        const error = { message: ERROR_INVALID_CITY };
+        throw(error);
+    }
+
+    const baseUrl = BASE_URL_WEATHER + "/forecast";
+    const params = `q=${city_}`;
+    return await fetchData(baseUrl, params);
+}
