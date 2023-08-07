a931dd37c234782bf3f1f14cde4b6cd7064cfdc1
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-07 12:06:25 +0500

    Added CurrentWidget & DailyWidget Components

--- src/components/widgets/CurrentWidget.jsx
+++ src/components/widgets/CurrentWidget.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { useWeather } from '../../providers/weatherContext';
+import {
+  convertKelvinToFahrenheit,
+  getWeatherIcon,
+  resetApp
+} from "../../utils";
+import Loader from '../Loader';
 
 const CurrentWidget = () => {
+  const { weatherData, copyShareUrl } = useWeather();
+
+  if (!weatherData || !weatherData.data) {
+    return <Loader />
+  }
+
+  if (weatherData || Object.keys(weatherData.data).length === 0) {
+    return <Loader />
+  }
+  const fullWeatherInfo = weatherData.data;
+  const { main, name: location, weather} = fullWeatherInfo;
+  const { temp, feels_like, humidity, pressure, temp_max, temp_min } = main;
+  const { main: mainDetail, icon, description } = weather[0];
+
+
   return (
-    <div>CurrentWidget</div>
+    <>
+      <div className="widget weather-detail">
+        <div className="widget-actions">
+          <button onChange={resetApp}>Reset</button>
+          <button onChange={copyShareUrl}>Share</button>
+        </div>
+        <h2>{location}</h2>
+        <img src={getWeatherIcon(temp)} alt={icon} />
+        <p>{description}</p>
+        <h3>{convertKelvinToFahrenheit(temp)}º</h3>
+        <p>{mainDetail}</p>
+      </div>
+      <div className="weather-extra-wrapper my-other-step">
+        <div className="widget weather-extra bg-extra1">
+          <p><span>Temp</span> | <span>{convertKelvinToFahrenheit(temp)}º</span></p>
+          <p><span>Feels like</span> | <span>{convertKelvinToFahrenheit(feels_like)}º</span></p>
+        </div>
+        <div className="widget weather-extra bg-extra2">
+          <p><span>Humidity</span> | <span>{humidity}</span></p>
+          <p><span>Pressure</span> | <span>{pressure}</span></p>
+        </div>
+        <div className="widget weather-extra bg-extra3">
+          <p><span>Temp max</span> | <span>{convertKelvinToFahrenheit(temp_max)}</span></p>
+          <p><span>Temp min</span> | <span>{convertKelvinToFahrenheit(temp_min)}</span></p>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/widgets/DailyWidget.jsx
+++ src/components/widgets/DailyWidget.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useWeather } from '../../providers/weatherContext'
+import DailyDetail from './DailyDetail'
+import Loader from '../Loader';
 
 const DailyWidget = () => {
+  const { forecast } = useWeather();
+
+  if(!forecast || !forecast.data) {
+    return <Loader />
+  }
+
+  if (!forecast.loading || Object.keys(forecast.data).length === 0) {
+    return <Loader />
+  }
+  
+  const {list: forecastList } = forecast.data;
+
+
   return (
-    <div>DailyWidget</div>
+    <>
+      <h3 className="widget-title">Forecast</h3>
+      <div className="daily-container">
+        <div className="daily-wrapper">
+          {forecastList && forecastList.map(item => <DailyDetail key={item.dt} data={item} /> )}
+        </div>
+      </div>
+    </>
   )
 }
 
